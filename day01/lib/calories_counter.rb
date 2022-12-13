8d1e494adf3c7f4e0bb841ff6f8a16bdab6561c8
3rick <erick.dejesus1@alumno.buap.mx>
2022-12-13 09:17:59 -0600

    add: calories_counter class

---
+++ day01/lib/calories_counter.rb
@@ -0,0 +1,16 @@
+class CaloriesCounter
+  def calculate(line)
+    line.split("\n").collect{ |n| n.to_i }.sum
+  end
+
+  def most_calories( elves_list, num=1 )
+    elves_list.split("\n\n").collect{ |n| calculate(n) }.max(num).sum
+  end
+end
+
+if __FILE__ == $0
+  elves_list = File.read("input.txt")
+  counter = CaloriesCounter.new
+  most_calories = counter.most_calories( elves_list, 3)
+  p most_calories
+end

04ce1468ca7404d3b40f594ed8023866ed1655b8
3rick <erick.dejesus1@alumno.buap.mx>
2022-12-14 16:35:13 -0600

    add: RucksackSorter#common_items

--- day03/lib/rucksack_sorter.rb
+++ day03/lib/rucksack_sorter.rb
@@ -2,4 +2,7 @@ class RucksackSorter
   def compartments(list)
     [list[0,list.size/2],list[list.size/2,list.size]]
   end
+  def common_items(first, second)
+    first.split("").intersection(second.split("")).pop
+  end
 end

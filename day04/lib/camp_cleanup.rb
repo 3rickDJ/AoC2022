e3936b0521fb6562e07241ee6ae942b0923ad686
3rick <erick.dejesus1@alumno.buap.mx>
2022-12-16 10:01:02 -0600

    add: CampCleanup#contain?

--- day04/lib/camp_cleanup.rb
+++ day04/lib/camp_cleanup.rb
@@ -5,4 +5,17 @@ class CampCleanup
             a..b
         end
     end
+
+    def contain?(ranges)
+        ranges = sections(ranges)
+        a = ranges[0].to_set
+        b = ranges[1].to_set
+        a.subset?(b) || b.subset?(a)
+    end
+end
+
+if __FILE__ == $0
+    assignments = File.readlines("input.txt")
+    campCleanup = CampCleanup.new
+    assignments.select{|n| campCleanup.contain?(n) }.size
 end

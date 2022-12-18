1232a94f49286698e664dc8762a1115372f270c2
3rick <erick.dejesus1@alumno.buap.mx>
2022-12-18 12:11:55 -0600

    Me tengo que ir a despejar

--- day05/lib/supply_stacks.rb
+++ day05/lib/supply_stacks.rb
@@ -1,2 +1,15 @@
 class SupplyStacks
+  attr_reader :stacks
+  def initialize(input)
+    @input = input
+    @stacks = []
+  end
+  def parse_input
+    # @input.scan(/\ \d\ \ /).size
+    stacks = @input.split("\n").find_index{ |n| n.match? /\ \d\ \ / } - 1
+    lines = @input.split("\n")[stacks]
+    lines.each_line do |line|
+      crates = line.scan(/\[(.)\]/)
+    end
+  end
 end

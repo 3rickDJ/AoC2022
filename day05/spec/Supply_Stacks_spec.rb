f506aaa45255286a48f1f91a47ad3ceca299080b
3rick <erick.dejesus1@alumno.buap.mx>
2022-12-18 09:47:56 -0600

    fix: define input variable before test #stacks

--- day05/spec/Supply_Stacks_spec.rb
+++ day05/spec/Supply_Stacks_spec.rb
@@ -1,8 +1,9 @@
-require 'Supply_Stacks'
+require 'supply_stacks'
 
 describe SupplyStacks do
   describe "Part I" do
-    before do
+    context "Divide instruction from input" do
+      it "returns 3 arrays" do
         input = <<~EOT
             [D]
         [N] [C]
@@ -14,9 +15,6 @@ describe SupplyStacks do
         move 2 from 2 to 1
         move 1 from 1 to 2
         EOT
-    end
-    context "Divide instruction from input" do
-      it "returns 3 arrays" do
         stack = SupplyStacks.new
         expect(stack.stacks(input)).to eq [["Z","N"],["M","C","D"],["P"]]
       end

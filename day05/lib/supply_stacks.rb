1c41b56a5f475968d970cbda86e3d8027c85c054
3rick <erick.dejesus1@alumno.buap.mx>
2023-01-05 10:31:35 -0600

    quien sabe con que hago

--- day05/lib/supply_stacks.rb
+++ day05/lib/supply_stacks.rb
@@ -2,14 +2,22 @@ class SupplyStacks
   attr_reader :stacks
   def initialize(input)
     @input = input
-    @stacks = []
+    @stacks = parse_input(input)
   end
-  def parse_input
-    # @input.scan(/\ \d\ \ /).size
-    stacks = @input.split("\n").find_index{ |n| n.match? /\ \d\ \ / } - 1
-    lines = @input.split("\n")[stacks]
-    lines.each_line do |line|
-      crates = line.scan(/\[(.)\]/)
-    end
+  def parse_input(input)
+    lines = input.split("\n")
+    num_stacks = lines[-1].each_with_index.select{|n,i| n != ' '}.collect{ |n| n[1]}
+    stacks = num_stacks.collect{ |n| lines.collect{ |a| a[n] } }
+    @stacks = stacks.collect do |n|
+      n.select do |a|
+           if a== " "
+             false
+           elsif a.match? /\d/
+             false
+           else
+             true
+           end
+         end
+      end
   end
 end

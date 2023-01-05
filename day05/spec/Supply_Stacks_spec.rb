d0cf1009986036eade7c73fa4211d4f40410d6a4
3rick <erick.dejesus1@alumno.buap.mx>
2023-01-05 09:29:05 -0600

    test: update, SupplyStacks#new

--- day05/spec/Supply_Stacks_spec.rb
+++ day05/spec/Supply_Stacks_spec.rb
@@ -4,17 +4,12 @@ describe SupplyStacks do
   describe "Part I" do
     context "Divide instruction from input" do
       it "returns 3 arrays" do
-        input = <<~EOT
-            [D]
-        [N] [C]
+        input = <<~TEXT
+            [D]    
+        [N] [C]    
         [Z] [M] [P]
-         1   2   3
-
-        move 1 from 2 to 1
-        move 3 from 1 to 3
-        move 2 from 2 to 1
-        move 1 from 1 to 2
-        EOT
+         1   2   3 
+        TEXT
         stack = SupplyStacks.new(input)
         expect(stack.stacks).to eq [["Z","N"],["M","C","D"],["P"]]
       end

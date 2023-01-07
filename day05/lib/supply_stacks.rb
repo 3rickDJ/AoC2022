fe5205737dcf58a775894b22e5c49ea1f474dec0
3rick <erick.dejesus1@alumno.buap.mx>
2023-01-06 22:36:50 -0600

    add: SupplyStacks#parse_input

--- day05/lib/supply_stacks.rb
+++ day05/lib/supply_stacks.rb
@@ -5,19 +5,30 @@ class SupplyStacks
     @stacks = parse_input(input)
   end
   def parse_input(input)
-    lines = input.split("\n")
+    lines = input.split("\n").map{|n| n.split ""}
     num_stacks = lines[-1].each_with_index.select{|n,i| n != ' '}.collect{ |n| n[1]}
     stacks = num_stacks.collect{ |n| lines.collect{ |a| a[n] } }
     @stacks = stacks.collect do |n|
       n.select do |a|
-           if a== " "
-             false
-           elsif a.match? /\d/
-             false
-           else
-             true
-           end
-         end
-      end
+        if a== " "
+          false
+        elsif a.match? /\d/
+          false
+        else
+          true
+        end
+      end.reverse
+    end
   end
+end
+
+if $0 == __FILE__
+        input = <<~TEXT
+            [D]    
+        [N] [C]    
+        [Z] [M] [P]
+         1   2   3 
+        TEXT
+        stack = SupplyStacks.new(input)
+        p stack.stacks
 end
